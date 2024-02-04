fe048a2593450b011884b48a314fb77f3d7aba5e
rontolero28 <rontolero28@icloud.com>
2024-02-04 22:33:12 +0900

    placed the password in an environmental variable

--- send_email.py
+++ send_email.py
@@ -17,19 +17,19 @@
 #     server.sendmail(user, receiver, message)
 
 import smtplib
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 587
 
     user = "rontolero628@gmail.com"
-    password = "lnfsysmyzjkbpvkn"
+    password = os.environ.get("PASSWORD") 
 
     receiver = "rontolero628@gmail.com"
 
     server = smtplib.SMTP(host, port)
     server.starttls()
 
-    server.login(user=user, password=password )
+    server.login(user=user, password=password)
     server.sendmail(user, receiver, message)
