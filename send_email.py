14efada031006a54e3a55d36c5e8f5e7e9720652
rontolero28 <rontolero28@icloud.com>
2024-02-05 00:21:31 +0900

    placed the password in an environmental variable

--- send_email.py
+++ send_email.py
@@ -1,21 +1,3 @@
-# import smtplib, ssl (this code was shown in the lesson but does not work. always show unocode　error)
-#
-# # def send_email(message):
-# host = "smtp.gmail.com"
-# port = 465
-#
-# user = "rontolero628＠gmail.com"
-# password = "lnfsysmyzjkbpvkn"
-#
-# receiver = "rontolero628@gmail.com"
-# my_context = ssl.create_default_context()
-#
-# message = "Hi There and Bye"
-#
-# with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-#     server.login(user, password)
-#     server.sendmail(user, receiver, message)
-
 import smtplib
 import os
 
