f75c5e7f672b2ce360863cdb3d06c6723953a50b
rontolero28 <rontolero28@icloud.com>
2024-02-04 17:23:30 +0900

    Send emails using form

--- send_email.py
+++ send_email.py
@@ -1,18 +1,32 @@
-import smtplib, ssl
+# import smtplib, ssl (this code was shown in the lesson but does not work. always show unocode　error)
+#
+# # def send_email(message):
+# host = "smtp.gmail.com"
+# port = 465
+#
+# user = "rontolero628＠gmail.com"
+# password = "lnfsysmyzjkbpvkn"
+#
+# receiver = "rontolero628@gmail.com"
+# my_context = ssl.create_default_context()
+#
+# message = "Hi There and Bye"
+#
+# with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+#     server.login(user, password)
+#     server.sendmail(user, receiver, message)
 
-# def send_email(message):
-host = "smtp.gmail.com"
-port = 465
+import smtplib
 
-username = "rontolero628＠gmail.com"
-password = "tktgvhqwphiyogdp"
 
-receiver = "rontolero628@gmail.com"
-my_context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 587
 
-message = """Hi there"""
+    receiver = "rontolero628@gmail.com"
 
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    server = smtplib.SMTP(host, port)
+    server.starttls()
 
+    server.login("rontolero628@gmail.com", "lnfsysmyzjkbpvkn")
+    server.sendmail("rontolero628@gmail.com", receiver, message)
